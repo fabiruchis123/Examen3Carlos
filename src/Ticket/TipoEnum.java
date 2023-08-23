7122fef6aef00f839272cdada55d2f6556a4cc19
CarlosMorales <cfmorales191@gmail.com>
2023-08-23 09:51:47 -0600

    Tipo Enum
    Se crea la clase Enum de tipo

---
+++ src/Ticket/TipoEnum.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package Ticket;
+
+/**
+ *
+ * @author Student
+ */
+public enum TipoEnum {
+    
+    P,
+    C,
+    A
+    
+}
