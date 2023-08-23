3c1c17fc61467ec74ad4b23de165bf7c401eb1c9
CarlosMorales <cfmorales191@gmail.com>
2023-08-23 10:44:50 -0600

    TicketList
     Se crea el ticket list con los metodos de busqueda, eliminacion y agregacion

---
+++ src/TicketList/TicketList.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TicketList;
+
+import Ticket.TicketModel;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Student
+ */
+public class TicketList {
+    
+    private ArrayList<TicketModel> lista;
+    
+    
+ public void Agregar(TicketModel ticket){
+    
+     lista.add(ticket);
+     
+ }
+ 
+ public void EliminacionPos(int pos){
+     
+     lista.remove(pos);
+     
+ }
+ 
+ public void EliminacionValor(TicketModel ticket){
+     
+     lista.remove(ticket);
+     
+ }
+ 
+ public TicketModel Busqueda(enum tipobuscado){
+     
+     for(TicketModel tickets : lista){ 
+         
+         if (tickets.getTipoEnum()== tipobuscado) {
+             return TicketModel;
+         }
+         
+     }
+ }
+    
+}
